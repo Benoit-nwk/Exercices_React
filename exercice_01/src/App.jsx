140ebe9691a3b4371ebee39c83d3d598bc160408
benoit <bntnowak@outlook.com>
2025-03-06 12:15:41 +0100

    exercice01 terminé

--- exercice_01/src/App.jsx
+++ exercice_01/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Tab from './components/Tab/Tab'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Tab/>
     </>
   )
 }
-
 export default App
